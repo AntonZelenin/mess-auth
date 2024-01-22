76413cce418bb6aa9347d7708d1e9236288b285f
Anton Zelenin <ntnzelenin@gmail.com>
2024-01-22 18:02:14 +0200

    Return custom response for form validation

--- mess_auth/main.py
+++ mess_auth/main.py
@@ -1,10 +1,14 @@
+from collections import defaultdict
 from datetime import timedelta
 from typing import Annotated, Optional
 
 from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError
 from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
 from mess_auth import repository, schemas, utils, constants
 from mess_auth.models.user import User
@@ -25,6 +29,23 @@ def authenticate_by_creds(username: str, password: str) -> Optional[User]:
         return user
 
     return None
+
+
+@app.exception_handler(RequestValidationError)
+async def custom_form_validation_error(_, exc):
+    reformatted_message = defaultdict(list)
+    for pydantic_error in exc.errors():
+        loc, msg = pydantic_error["loc"], pydantic_error["msg"]
+        filtered_loc = loc[1:] if loc[0] in ("body", "query", "path") else loc
+        field_string = ".".join(filtered_loc)  # nested fields with dot-notation
+        reformatted_message[field_string].append(msg)
+
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder(
+            {"errors": reformatted_message}
+        ),
+    )
 
 
 @app.get("/api/v1/authorize")
